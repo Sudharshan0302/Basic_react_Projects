f0dc252c239b068eb095db0b82c765a8ef4a5ccc
Sudharshan0302 <sudharshanrcsusan6@gmail.com>
2024-12-26 21:45:06 +0530

    props concept

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,8 +1,18 @@
-
+import Home from "./Home"
+import Contact from "./Contact";
+import About from "./About"
+import Greeting from "./Greeting";
 function App() {
   return (
-    <h1>Hello</h1>
+    <div>
+    <h1>Hello from ReactJS</h1>
+    <Greeting name = {'Sudharshan'}/>
+    <Greeting name = {'RCS'}/>
+    {/* <Home name = {'Sudharshan'} age = {24} />
+    <About />
+    <Contact /> */}
+
+    </div>
   );
 }
-
 export default App;
---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+    <>
+    <h1>Header Component</h1>
+    <header>Some header Component</header>
+    </>
+    )
+}
+export default Header;
